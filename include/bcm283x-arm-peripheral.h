daaff9a370d715613ca1d17cdcdc99cc46247d4a
Paul Hoffman <pvhoffman@gmail.com>
2018-05-24 18:13:41 -0700

    fixed bugs for interfacing with GPIO

--- include/bcm283x-arm-peripheral.h
+++ include/bcm283x-arm-peripheral.h
@@ -12,9 +12,14 @@ namespace bcm283x_arm_peripheral
             static constexpr uint32_t base_peripheral_physical_address = 0x3f000000;
             static constexpr uint32_t base_gpio_physical_address = base_peripheral_physical_address + 0x200000;
             static constexpr uint32_t memory_block_size = 4096;
+            static constexpr uint32_t memory_page_size  = 4096;
+            static constexpr uint32_t gpfsel0 = 0;
             static constexpr uint32_t gpset0 = 7;
             static constexpr uint32_t gpclr0 = 10;
             static constexpr uint32_t gplev0 = 13;
+            static constexpr uint32_t gppud  = 37;
+            static constexpr uint32_t gppudclk0 = 38;
+            static constexpr uint32_t cycle_150 = 150;
             static constexpr const char* memory_device_path = "/dev/mem";
             constexpr const_defs(){}
     };
@@ -28,24 +33,68 @@ namespace bcm283x_arm_peripheral
                     volatile uint32_t* _paddr;
                     int32_t _num;
                     pin_t() {}
+
+                    void _wait(const uint32_t c)
+                    {
+                        for(int i = 0; i < c; i++)
+                        {
+                            // volatile prevents optimization
+                            // out of the loop.
+                            asm volatile("nop");
+                        }
+                    }
                 public:
                     enum class pin_direction { input, output };
+                    enum class pin_pullstate { off = 0, pulldown, pullup };
+                    enum class pin_initialstate {none, high, low };
                     pin_t(void* ptr, const int32_t n) : _num(n)
                     {
                         _paddr = static_cast<volatile uint32_t*>(ptr);
                     }
-                    void set_direction(pin_direction dir)
+                    void setup(pin_direction direction = pin_direction::output, pin_pullstate pullstate = pin_pullstate::off, pin_initialstate initialstate = pin_initialstate::high)
                     {
-                        const uint32_t mem_offset = _num / 10;
-                        const uint32_t bit_offset = (_num % 10) * 3;
+                        if(direction == pin_direction::output)
+                        {
+                            if(pullstate != pin_pullstate::off)
+                            {
+                                throw std::runtime_error("Output pin cannot have pullup/pulldown");
+                            }
+                            // explicitly disable pullup/pulldown
+                            uint32_t offset = const_defs::gppudclk0 + (_num / 32);
+                            *(_paddr + const_defs::gppud) &= ~3;
+                            _wait(const_defs::cycle_150);
+                            *(_paddr + offset) = 1 << (_num % 32);
+                            _wait(const_defs::cycle_150);
+                            *(_paddr + const_defs::gppud) &= ~3;
+                            *(_paddr + offset) = 0;
 
-                        if(dir == pin_direction::input)
-                        {
-                            *(_paddr + mem_offset) &= ~(7 << bit_offset);
+                            // set the inital state to high/low
+                            offset = (initialstate == pin_initialstate::high) ? const_defs::gpset0 : const_defs::gpclr0;
+                            offset += (_num / 32);
+                            *(_paddr + offset) = 1 << (_num % 32);
+
+                            // set pin as output
+                            offset = const_defs::gpfsel0 + (_num / 10);
+                            *(_paddr + offset) = (*(_paddr + offset) & ~(7 << ((_num % 10) * 3))) | (1 << ((_num % 10) * 3));
                         }
-                        else if(dir == pin_direction::input)
+                        else
                         {
-                            *(_paddr + mem_offset) |= (1 << bit_offset);
+                            if(initialstate != pin_initialstate::none)
+                            {
+                                throw std::runtime_error("Initial high/low not valid for input pin.");
+                            }
+                            // set pullup/pulldown
+                            uint32_t offset = const_defs::gppudclk0 + (_num / 32);
+                            *(_paddr + const_defs::gppud) = (*(_paddr + const_defs::gppud) & ~3) | static_cast<uint32_t>(pullstate);
+                            _wait(const_defs::cycle_150);
+                            *(_paddr + offset) = 1 << (_num % 32);
+                            _wait(const_defs::cycle_150);
+                            *(_paddr + const_defs::gppud) &= ~3;
+                            *(_paddr + offset) = 0;
+
+                            // set pin as input
+                            offset = const_defs::gpfsel0 + (_num / 10);
+                            *(_paddr + offset) = (*(_paddr + offset) & ~(7 << ((_num % 10) * 3)));
                         }
                     }
                     void set() 
@@ -64,7 +113,9 @@ namespace bcm283x_arm_peripheral
 
         private:
             int32_t     _map_fd;
-            void*       _pmap;
+            uint8_t*    _pmap;
+            uint8_t*    _pmap_mem;
+            uint32_t*   _gpio_map;
 
             void _open_memory_device();
             void _map_memory_device();
--- src/bcm283x-arm-peripheral.cxx
+++ src/bcm283x-arm-peripheral.cxx
@@ -9,7 +9,7 @@
 namespace bcm283x_arm_peripheral
 {
 //---------------------------------------------------------
-gpio_t::gpio_t() : _map_fd(0), _pmap(nullptr)
+gpio_t::gpio_t() : _map_fd(0), _pmap(nullptr), _pmap_mem(nullptr), _gpio_map(nullptr)
 {
     _open_memory_device();
     _map_memory_device();
@@ -19,12 +19,19 @@ gpio_t::~gpio_t()
 {
     if(_pmap != nullptr)
     {
-        munmap(_pmap, const_defs::memory_block_size);
+        munmap(_gpio_map, const_defs::memory_block_size);
         _pmap = nullptr;
     }
     if(_map_fd > 0)
     {
         close(_map_fd);
+        _map_fd = 0;
+    }
+    if(_pmap_mem)
+    {
+        delete [] _pmap_mem;
+        _pmap_mem = nullptr;
+
     }
 }
 //---------------------------------------------------------
@@ -43,21 +50,36 @@ void gpio_t::_map_memory_device()
     {
         throw std::runtime_error("Memory device not open.");
     }
-    _pmap = mmap(nullptr
+
+    // allocate and align on page boundry
+    _pmap_mem = new uint8_t[const_defs::memory_block_size + (const_defs::memory_page_size -1)];
+    if(nullptr == _pmap_mem)
+    {
+        throw std::runtime_error("Cannot allocate memory to map virtual address space.");
+    }
+    _pmap = _pmap_mem;
+    uint32_t boundry = reinterpret_cast<uint32_t>(_pmap_mem);
+    if(boundry % const_defs::memory_page_size)
+    {
+        _pmap += const_defs::memory_page_size - (boundry % const_defs::memory_page_size);
+    }
+    // map to the gpio location
+    void* memmap = mmap(static_cast<void*>(_pmap)
             , const_defs::memory_block_size
             , PROT_READ | PROT_WRITE
-            , MAP_SHARED
+            , MAP_SHARED | MAP_FIXED
             , _map_fd
             , const_defs::base_gpio_physical_address);
-    if(_pmap == nullptr)
+    if(MAP_FAILED == memmap)
     {
         throw std::runtime_error("Cannot map memory location.");
     }
+    _gpio_map = static_cast<uint32_t*>(memmap);
 }
 //---------------------------------------------------------
 gpio_t::pin_t gpio_t::pin(const uint32_t n)
 {
-    gpio_t::pin_t res(_pmap, n);
+    gpio_t::pin_t res(_gpio_map, n);
     return res;
 }
 //---------------------------------------------------------
